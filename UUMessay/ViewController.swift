deebfdd00f9434a662bb2699774021b74c8a40c8
YuyuQianRice <Yuyu.Qian.Rice@gmail.com>
2020-10-10 01:01:23 -0500

    Initial Commit

---
+++ UUMessay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UUMessay
+//
+//  Created by Yuyu Qian on 10/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
